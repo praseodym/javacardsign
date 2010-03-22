9a92e240c0321d8f6b49fa0e3c3e60ffd60717f4
pisi <pisi@9e10f836-6c2a-4035-9da3-5f9eb4517992>
2010-03-22 13:04:42 +0000

    * correctly use GP211 in variable name  * install with default selected privilege

--- pkiapplet/reinstall.sh
+++ pkiapplet/reinstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # re-install the applet to the card. You need gpj.jar.
 java -jar gpj.jar -deletedeps -delete  A000000063 -list
-java -jar gpj.jar -load bin/net/sourceforge/javacardsign/applet/javacard/applet.cap -install -list
+java -jar gpj.jar -load bin/net/sourceforge/javacardsign/applet/javacard/applet.cap -install -priv 4 -list
